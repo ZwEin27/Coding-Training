ecd9809f0b4197610ea17fba2776571f5405c0cc
ZwEin <zwein27@gmail.com>
2016-02-01 22:22:51 -0800

    326

---
+++ python/lc326.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0: return False
+        import math
+        return (math.log10(n)/math.log10(3)) % 1 == 0
+        # log10 not log
