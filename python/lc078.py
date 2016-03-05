2509a0ecab807281d84163fde206515de31c9ed2
ZwEin <zwein27@gmail.com>
2016-03-04 20:42:50 -0800

    078 question left

--- python/lc078.py
+++ python/lc078.py
@@ -4,23 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        size = len(nums)
-        return self.combination(1, size, 2)
 
-    def combination(self, start, end, k):
+        size = len(nums)
+        # result = self.combination(1, size, 0)
+        result = []
+        for i in range(0, size+1):
+            result.extend(self.combination(nums, 0, size, i)) 
+        return result
+
+    def combination(self, nums, start, end, k):
         ans = []
         if k >= 2:
             for i in range(start, end): # 1,2,3 - 3 times
-                rels = self.combination(i+1, end, k-1)
+                rels = self.combination(nums, i+1, end, k-1)
                 for rel in rels:
-                    tmp = [i]
+                    tmp = [nums[i]]
                     tmp.extend(rel)
                     ans.append(tmp)
+        elif k == 1:
+            for i in range(start, end):
+                ans.append([nums[i]])
         else:
-            for i in range(start, end+1):
-                ans.append([i])
+            ans.append([])
+        
         return ans
 
 if __name__ == '__main__':
     s = Solution()
-    print s.subsets([1,2,3])
+    print s.subsets([4,1,0])
--- python/lc199.py
+++ python/lc199.py
@@ -19,11 +19,11 @@ class Solution(object):
         queue.append((root, 0))
 
         while len(queue) > 0:
-            cur, level = queue.pop()
+            cur, level = queue.pop(0)
             result.setdefault(level, 0)
             result[level] = cur.val
             if cur.left:
                 queue.append((cur.left, level+1))
             if cur.right:
-            queue.append((cur.right, level+1))
+                queue.append((cur.right, level+1))
         return result.values()
