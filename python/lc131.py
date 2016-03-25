fca15e46840bab650911585b40e92f205816cf1f
ZwEin <zwein27@gmail.com>
2016-03-25 11:13:25 -0700

    131

---
+++ python/lc131.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/discuss/78261/python-recursive-iterative-backtracking-solution
+
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        res = []
+        self.dfs(s, [], res)
+        return res
+
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path)
+            return
+        for i in range(1, len(s)+1):
+            if self.isPal(s[:i]):
+                self.dfs(s[i:], path+[s[:i]], res)
+
+    def isPal(self, s):
+        return s == s[::-1]
+
+
+s = 'aabb'
+print Solution().partition(s)
+
+# n = 'aba'
+# print str(n)[::-1]
+
+# print str(n) == str(n)[::-1]
