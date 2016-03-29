06ace711f06f7ad2fd72e7a44c32bd8c79d75c9d
ZwEin <zwein27@gmail.com>
2016-03-28 21:10:04 -0700

    49

--- python/lc049.py
+++ python/lc049.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/anagrams/
 # begin for problem
+
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        d = {}
+        for w in sorted(strs):
+            key = tuple(sorted(w))
+            d[key] = d.get(key, []) + [w]
+        return d.values()
+
+
+strs = ["tea","and","ate","eat","den"]
+print Solution().groupAnagrams(strs)
