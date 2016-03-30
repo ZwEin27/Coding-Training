b47bd1ffb667bdea4a954b3fe33bebe73ab698b7
ZwEin <zwein27@gmail.com>
2016-03-30 11:00:31 -0700

    322

---
+++ python/lc322.py
@@ -0,0 +1,25 @@
+#   http://bookshadow.com/weblog/2015/12/27/leetcode-coin-change/
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        
+        dp = [0] + [-1] * amount
+
+
+        for x in range(amount+1):
+            for c in coins:
+                if x+c <= amount:
+                    if dp[x+c] < 0 or dp[x+c] > dp[x]+1:
+                        dp[x+c] = dp[x] + 1
+                    
+        return dp[-1] 
+
+
+coins = [1,2,5]
+amount = 11
+print Solution().coinChange(coins, amount)
