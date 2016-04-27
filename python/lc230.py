fc44e63048174d259738b6024b41dd66303c9e20
ZwEin <zwein27@gmail.com>
2016-04-27 10:22:41 -0700

    230

---
+++ python/lc230.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+
+        stack = []
+        node = root
+        while node:
+            stack.append(node)
+            node = node.left
+
+        idx = 1
+        while stack and idx <= k:
+            node = stack.pop(-1)
+            tmp = node.right
+            while tmp:
+                stack.append(tmp)
+                tmp = node.left
+        return node.val
