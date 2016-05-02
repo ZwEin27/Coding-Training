ca0c8949f5c5d21c8677e968910152f8eb2d6b49
ZwEin <zwein27@gmail.com>
2016-05-01 21:04:39 -0700

    164

---
+++ python/lc164.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        size = len(nums)
+        if size <= 2:
+            return 0
+        nums.sort()
+        max_value = 0
+        for i in range(1, size):
+            gap = nums[i] - nums[i-1]
+            if gap > max_value:
+                max_value = gap
+        return max_value
