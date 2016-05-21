b4b151577c43100222e01e6b1543a81a49f0f09d
ZwEin <zwein27@gmail.com>
2016-05-21 15:33:22 -0700

    test

--- LeetCode/python/lc000.py
+++ LeetCode/python/lc000.py
@@ -1,16 +1,35 @@
-# # Test part
-#
+# Test part
+
 # test = {
-#     '1': 's',
-#     '2': '1'
+#     '1': '0.1',
+#     '2': '0.2'
 # }
-# test = sorted(test, key=lambda x: x[0], reverse=True)
+# test = sorted(test.iteritems(), key=lambda x: x[1], reverse=True)
+# test = [int(_[0]) for _ in test]
 #
 # # tmp = test.get(1)
 # # tmp.extend([4,5,6])
 # print test
 
+# x = [4,2,4,5,3,7]
+# for cur, val in enumerate(x):
+#     print cur, val
 
-string = '{12}'
+import numpy as np
 
-print string[1:-1]
+# print np.random.randint(2, size=1)
+
+# tmp = np.random.random_integers(0, 1, size=2)
+# print tmp
+# print np.where(tmp)
+# print np.random.random_integers(0, 1, size=2)
+
+# size = 100
+# tmp = np.arange(size)
+# np.random.shuffle(tmp)
+# print tmp[:int(size*.1)]
+
+# l = [2,2,2]
+# print l.index(2)
+
+print 1.*11/27
