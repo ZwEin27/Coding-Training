2f39a520ea6b6fd4c4611ae88f64db48ff3b7cb7
ZwEin <zwein27@gmail.com>
2016-08-26 15:02:52 -0700

    add explanation

--- LeetCode/python/lc372.py
+++ LeetCode/python/lc372.py
@@ -1,4 +1,13 @@
 class Solution(object):
+    """
+    One knowledge: ab % k = (a%k)(b%k)%k
+    Since the power here is an array, we'd better handle it digit by digit.
+    One observation:
+    a^1234567 % k = (a^1234560 % k) * (a^7 % k) % k = (a^123456 % k)^10 % k * (a^7 % k) % k
+    Looks complicated? Let me put it other way:
+    Suppose f(a, b) calculates a^b % k; Then translate above formula to using f :
+    f(a,1234567) = f(a, 1234560) * f(a, 7) % k = f(f(a, 123456),10) * f(a,7)%k;
+    """
     # https://www.hrwhisper.me/leetcode-super-pow/
     # https://discuss.leetcode.com/topic/50489/c-clean-and-short-solution
     def superPow(self, a, b):
